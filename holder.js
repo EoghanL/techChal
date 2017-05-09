474bbc6910d5dd36dc1edbb3a57e73b4ed197eed
github username <github email address>
2017-05-09 13:53:30 -0400

    pieces expanding

--- holder.js
+++ holder.js
@@ -1,7 +1,7 @@
 let request = new XMLHttpRequest();
 let url = 'https://marmot.com/cart';
 request.responseType= 'document'
-request.open("GET", url, true)            //////// THIS WORKS \\\\\\\\\\\\
+request.open("GET", url, true)            //////// skeleton jQuery request \\\\\\\\\\\\
 request.onreadystatechange = function() {
     if (this.readyState == 4) {
       let response = this.response
@@ -10,30 +10,33 @@ request.onreadystatechange = function() {
 };
 request.send()
 
-this.response.getElementsByClassName('cart-order-totals')
+this.response.getElementsByClassName('cart-order-totals') ///proper selectors 1 & 2
 this.response.getElementsByClassName('cart-row')
 
-// request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-//
-// $.ajax({
-//     url: `https://marmot.com/cart`,
-//     method: "GET",
-// 	  crossOrigin: true,
-//     done: function(data){
-//       let response = data
-//       debugger
-//     }
-//   })
 
 parseInt(document.getElementsByClassName('minicart-quantity')[1].innerHTML, 10)
 
 <div style={ position: 'fixed' z-index: '1'} />
 let contentOverlay = document.createElement('div')
 contentOverlay.style.height = `${window.innerHeight}px`
-contentOverlay.style.width = '100%'           /////////Getting there\\\\\\\\
+contentOverlay.style.width = '100%'           /////////over-lay div\\\\\\\\
 contentOverlay.style.backgroundColor = "rgba(0,0,0, 0.9)"
 contentOverlay.style.position = "fixed"
 contentOverlay.style.top = '0'
 contentOverlay.style.left = '0'
-document.body.appendChild(contentOverlay)
-contentOverlay.style.zIndex = '1'
+contentOverlay.style.zIndex = '10'
+document.body.prepend(contentOverlay)
+
+///========> document.body.removeChild(contentOverlay)
+
+function getPercentageScrolled(docHeight, yOffset){ ///yOffset is in the form of `window.innerHeight + window.pageYOffset`
+  return (yOffset / docHeight) * 100
+}
+getPercentageScrolled(document.body.offsetHeight, (window.innerHeight + window.pageYOffset))
+ //This properly parses the percentage
+ Math.round(getPercentageScrolled(document.body.offsetHeight, (window.innerHeight + window.pageYOffset)))
+
+
+
+
+ 
